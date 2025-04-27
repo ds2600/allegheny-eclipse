c21e09dc2712d2f8eefae86d52fb5c4d40f1876f
ds2600 <bill@ishiganto.com>
2025-04-27 00:47:21 -0400

    fixed nav links

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,19 +1,21 @@
 <?php
+$current_page = basename($_SERVER['PHP_SELF']);
+$base_url = ($current_page === 'index.php' || $current_page === '') ? '' : 'index.php';
 ?>
 
     <!-- Navbar -->
     <nav class="navbar">
         <div class="container">
             <div class="nav-container">
-                <a href="#hero" class="nav-logo">Allegheny Eclipse</a>
+                <a href="<?php echo $base_url; ?>#hero" class="nav-logo">Allegheny Eclipse</a>
                 <button class="nav-toggle" aria-label="Toggle navigation">
                     <span class="hamburger"></span>
                 </button>
                 <ul class="nav-menu">
-                    <li><a href="index.php#hero">Home</a></li>
-                    <li><a href="index.php#about">About</a></li>
-                    <li><a href="index.php#calendar">Calendar</a></li>
-                    <li><a href="index.php#contact">Contact</a></li>
+                    <li><a href="<?php echo $base_url; ?>#hero">Home</a></li>
+                    <li><a href="<?php echo $base_url; ?>#about">About</a></li>
+                    <li><a href="<?php echo $base_url; ?>#calendar">Calendar</a></li>
+                    <li><a href="<?php echo $base_url; ?>#contact">Contact</a></li>
                 </ul>
             </div>
         </div>
