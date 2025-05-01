ab727db703e3695c6bdf1edfc6fdbe6812483338
ds2600 <bill@ishiganto.com>
2025-05-01 07:46:02 -0400

    feat:added nav dropdowns

--- inc/navbar.php
+++ inc/navbar.php
@@ -13,7 +13,14 @@ $base_url = ($current_page === 'index.php' || $current_page === '') ? '' : 'inde
                 </button>
                 <ul class="nav-menu">
                     <li><a href="<?php echo $base_url; ?>#hero">Home</a></li>
-                    <li><a href="<?php echo $base_url; ?>#about">About</a></li>
+                    <li class="dropdown">
+                        <a href="<?php echo $base_url; ?>#about">About</a>
+<!--                        <ul class="dropdown-menu">
+                                <li><a href="about.html">Mission</a></li>
+                                <li><a href="<?php echo $base_url; ?>#team">Team</a></li>
+                            </ul>
+-->
+                    </li>
                     <li><a href="<?php echo $base_url; ?>#calendar">Calendar</a></li>
                     <li><a href="<?php echo $base_url; ?>#support">Support</a></li>
                     <li><a href="<?php echo $base_url; ?>#contact">Contact</a></li>
