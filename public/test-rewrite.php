c868280ff850c6104392945062302e2f55fed637
ds2600 <bill@ishiganto.com>
2025-07-16 15:02:26 -0400

    significant file cleanup

--- public/test-rewrite.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Display rewrite info
-echo "Rewrite Test Page\n";
-echo "----------------\n";
-echo "Requested URL: " . htmlspecialchars($_SERVER['REQUEST_URI']) . "\n";
-echo "Query Parameter 'url': " . (isset($_GET['url']) ? htmlspecialchars($_GET['url']) : 'Not set') . "\n";
-echo "Full Query String: " . htmlspecialchars($_SERVER['QUERY_STRING']) . "\n";
-echo "----------------\n";
-?>
